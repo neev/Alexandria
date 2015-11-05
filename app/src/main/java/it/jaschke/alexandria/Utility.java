1ecc78c8eb0a7058facc7842017a6215a9502fa1
neev <neeraja.vimal@hotmail.com>
2015-11-04 23:18:28 -0800

    Initial commit

---
+++ app/src/main/java/it/jaschke/alexandria/Utility.java
@@ -0,0 +1,26 @@
+package it.jaschke.alexandria;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by neeraja on 10/18/15.
+ */
+public class Utility {
+
+    /**
+     * Returns true if the network is available or about to become available.
+     *
+     * @param c Context used to get the ConnectivityManager
+     * @return
+     */
+    static public boolean isNetworkAvailable(Context c) {
+        ConnectivityManager cm =
+                (ConnectivityManager)c.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+    }
+}
